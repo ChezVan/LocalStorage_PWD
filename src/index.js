f7bcf94f3077b7410252ab3cdcd5c2756fae13bf
ChezVan <chezney@thedigitalacademy.co.za>
2023-04-29 20:54:34 +0200

    router added

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import Login from './Components/Login';
+import Register from './Components/Register';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    path: "/login",
+    element: <Login/>,
+    path: "/register",
+    element: <Register/>,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
