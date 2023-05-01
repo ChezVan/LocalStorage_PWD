6d52381333ab65c5168c4c9916237872217857a8
ChezVan <chezney@thedigitalacademy.co.za>
2023-05-01 21:38:15 +0200

    copy useState from register.jsx and paste in login.jsx

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 import Navbar from './Navbar'
 
 const Login = () => {
+
+  const [input, setInput] = useState //  check if name is exist to let login
+  ({
+      email: "",
+      password: ""
+  })
   return (
     <div>
         <Navbar/>
