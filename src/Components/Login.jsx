f778ffabe1719b8f31cdd1c6d9c2a5486e18c074
ChezVan <chezney@thedigitalacademy.co.za>
2023-05-01 22:16:27 +0200

    add login form

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,25 +1,62 @@
-import React from 'react'
-import Navbar from './Navbar'
+
+import '../App.css'
+import React, { useState } from 'react'
 
 const Login = () => {
 
   const [input, setInput] = useState //  check if name is exist to let login
-  ({
+    ({
       email: "",
       password: ""
-  })
+    })
 
-  const handleLogin = (e) =>
-  {
+  const handleLogin = (e) => {
     e.preventDefault();
     const loggeduser = localStorage.getItem("user");
 
   }
   return (
     <div>
-        <Navbar/>
+      <section className="vh-100 bg-image">
+        <div className="mask d-flex align-items-center h-100 gradient-custom-3">
+          <div className="container h-100">
+            <div className="row d-flex justify-content-center align-items-center h-100">
+              <div className="col-12 col-md-9 col-lg-7 col-xl-6">
+                <div className="card">
+                  <div className="card-body p-5">
+                    <h2 className="text-uppercase text-center mb-5">Login</h2>
 
-        login
+                    <form>
+
+                      <div className="form-outline mb-4">
+                        <input type="email" id="form3Example3cg" className="form-control form-control-lg" />
+                        <label className="form-label" htmlFor="form3Example3cg">Your Email</label>
+                      </div>
+
+                      <div className="form-outline mb-4">
+                        <input type="password" id="form3Example4cg" className="form-control form-control-lg" />
+                        <label className="form-label" htmlFor="form3Example4cg">Password</label>
+                      </div>
+
+                      <div className="d-flex justify-content-center">
+                        <button
+                          className="btn btn-success btn-block btn-lg gradient-custom-4 text-body">Login</button>
+                      </div>
+
+                      <p className="text-center text-muted mt-5 mb-0">Don't have an account? <a href="#!"
+                        className="fw-bold text-body"><u>Register here</u></a></p>
+
+                    </form>
+
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      login
     </div>
   )
 }
